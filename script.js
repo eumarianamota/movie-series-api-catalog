4363c30e95b5b77960cdf02a767cd904049767c2
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-11 14:33:27 -0300

    feat: implement card creation logic with movie and series details

--- script.js
+++ script.js
@@ -9,5 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("search");
 
   function criarCard(item, tipo, duration = "N/A"){
-    
-  }
+    const card = document.createElement(tipo == "movie" ? "movie-card" : "serie-card");
+    const title = item.title || item.name;
+    const overview = item.overview || "Sem sinopse disponível";
+    const generos = item.genre_ids || [];
+    const generoText = generos.join(",");
+
+    card.innerHTML =
+    <div class="img-cards">
+        <img src="${item.poster_path ? IMAGE_BASE + item.poster_path : '/public/Tela Pop.png'} " alt="${title}" />
+    </div>
+    <div class = "labels-container">
+        <div class ="label-card">${generoText || "N/A"}</div>
+    </div>
+    <h3>${title}</h3>
+    <p>${overview.substring(0, 150)}...</p>
+    <p>Duração: <span class = "duration">${duration}</span></p>
+    <button onclick = "window.open('https://www.themoviedb.org/${tipo}/${item.id}', '_blank')">Saber mais</button>
+    ;
+    return card;
+}
